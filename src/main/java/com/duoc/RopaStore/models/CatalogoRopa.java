a9359b18f6b4fe457c557389a45be3d33fca3612
LeoOlivaresD <cspraword@gmail.com>
2025-04-09 23:11:35 -0400

    Creacion de clase Modelo (CatalogoRopa)

--- src/main/java/com/duoc/RopaStore/models/CatalogoRopa.java
+++ src/main/java/com/duoc/RopaStore/models/CatalogoRopa.java
@@ -1,5 +1,16 @@
 package com.duoc.RopaStore.models;
 
-public class CatalogoRopa {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data // Esta notacion incluye metodos: Setters, Getters & toString
+@AllArgsConstructor // Indicamos que la clase tendra unc ontructor con todos los parametros
+@NoArgsConstructor // Indicamos que la clase tendra un constructor sin parametros
+public class CatalogoRopa {
+    private Long idRopa;
+    private String categoriaPrenda;
+    private String marcaPrenda;
+    private String tipoPrenda;
+    private int precioPrenda;
 }
