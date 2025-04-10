910595a1a9e1d0d6602b1550b52eb7f1e8aeb879
LeoOlivaresD <cspraword@gmail.com>
2025-04-10 00:10:09 -0400

    Se crea controlador con metodos Gets para listar de manera normal y listar por precio los productos del catalogo

--- src/main/java/com/duoc/RopaStore/controllers/RestControllerCatalogoRopa.java
+++ src/main/java/com/duoc/RopaStore/controllers/RestControllerCatalogoRopa.java
@@ -1,9 +1,38 @@
 package com.duoc.RopaStore.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.duoc.RopaStore.models.CatalogoRopa;
+import com.duoc.RopaStore.services.CatalogoRopaService;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/catalogo")
 public class RestControllerCatalogoRopa {
 
+    private final CatalogoRopaService catalogoRopaService;
+
+    @GetMapping
+    public ResponseEntity<List<CatalogoRopa>> listarRopa() {
+        List<CatalogoRopa> ropa = catalogoRopaService.getAllCatalogoRopa();
+        if (ropa.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(ropa);
+    }
+
+    @GetMapping("/precio")
+    public ResponseEntity<List<CatalogoRopa>> listarRopaPorPrecio() {
+        List<CatalogoRopa> ropa = catalogoRopaService.listarPorPrecioMenorAMayor();
+        if (ropa.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(ropa);
+    }
 }
